2804566d7b0a05bb5a8a3d69d8f811c9755a5c23
ABHISHEK CHARATE <118630322+ABHISHEKCHARATE@users.noreply.github.com>
2024-05-22 19:02:56 +0530

    requirement.txt

--- loginsystem/settings.py
+++ loginsystem/settings.py
@@ -74,27 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'loginsystem.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'summary',
-#         'USER': 'abhi',
-#         'PASSWORD': '123',
-#         'HOST': 'localhost',  # Or your PostgreSQL host
-#         'PORT': '5432',       # Or your PostgreSQL port
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
