2d45de26fb15155d6529876a7f69be49cdfb6f9f
ABHISHEK CHARATE <118630322+ABHISHEKCHARATE@users.noreply.github.com>
2024-05-23 17:56:42 +0530

    setting.py

--- loginsystem/settings.py
+++ loginsystem/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import whitenoise.storage
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'app_login.CustomUser'
 
